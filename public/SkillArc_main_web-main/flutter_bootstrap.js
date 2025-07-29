99ad0705d4d9c7291164d2c47e20fe04cad62819
bhasv <bhasvanth02@gmail.com>
2025-07-29 21:26:46 +0530

    chatbot added and student updated

--- public/SkillArc_main_web-main/flutter_bootstrap.js
+++ public/SkillArc_main_web-main/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3582083306"
+    serviceWorkerVersion: "2270962478"
   }
 });
