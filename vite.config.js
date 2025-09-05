208e8556c269bacaf53edb1bdd1604c433827f4f
Bhasvanth-Dev9380 <bhasvanth02@gmail.com>
2025-09-05 13:10:12 +0530

    deploy to pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
-  base: '/lumbini-technologies-website/',
+  base: './',
   server: {
     fs: {
       // Allow serving files from one level up to the project root
