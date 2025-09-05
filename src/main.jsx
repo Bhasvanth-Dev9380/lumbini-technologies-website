6d15e38555aeb13c181808a6850f9d8af6878a3b
Bhasvanth-Dev9380 <bhasvanth02@gmail.com>
2025-09-05 13:59:11 +0530

    deploy to pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
+// main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// --- Add this block ---
+const params = new URLSearchParams(window.location.search)
+const redirect = params.get('redirect')
+if (redirect) {
+  const newUrl = '/lumbini-technologies-website' + redirect
+  window.history.replaceState({}, '', newUrl)
+}
+// --- End block ---
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
